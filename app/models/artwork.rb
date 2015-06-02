281d92c9fc4de021b07d66d2a8470bd2217ebe30
Mai Tamakoshi <mai23coco@gmail.com>
2015-06-02 12:26:00 -0400

    added devise gem

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,8 +1,9 @@
 class Artwork < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  validates :title, presence: true
-  validates :history, presence: true, length: { maximum: 1000 }
+  validates :name, presence: true
   validates :location, presence: true
-  validates :
+  validates :history, presence: true, length: { maximum: 1000 }
+ has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
+        validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
 
