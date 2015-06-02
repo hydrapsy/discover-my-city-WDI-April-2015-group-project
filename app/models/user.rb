66d5bd500d0c9c9b2f97b47a52f72240ddbb5cba
victoriaj13@gmail.com <victoriaj13@gmail.com>
2015-06-02 10:25:10 -0400

    Added users model table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+	
+	has_many :comments, dependent: :destroy
+
+	validates :password, confirmation: true
+	validates_uniqueness_of :email
+	validates_presence_of :fname, :lname, :email, :password
+	validates :password, presence: true, on: :create
+	validates :password_confirmation, presence: true, on: :create
+
+
+	def self.find_by_email(email)
+		User.where("lower(email) = ?", email.downcase).first
+	end
+
 end
